a5ee2fd358435efaa81ded5771af2a69f02d7857
Vianney <vianneyjchin@gmail.com>
2019-11-23 15:34:57 -0800

    Fetch weather data

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,19 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text, Button } from 'react-native'
 
-export const HomeScreen = ({ navigation }) => (
-  <View>
-    <Button
-      title='Add a city'
-      onPress={() => navigation.navigate('AddCityScreen')}
-    />
-    <Button
-      title='Test Profile Page'
-      onPress={() =>
-        navigation.navigate('CityForecast', {
-          test: 'Cool weather data here'
-        })
-      }
-    />
-  </View>
-)
+export const HomeScreen = ({ navigation, resource }) => {
+  // useEffect(() => {
+  //   fetch(
+  //     'https://api.darksky.net/forecast/231063ebc5ecb280ea352c370e75450b/37.8267,-122.4233'
+  //   )
+  //     .then(res => res.json())
+  //     .then(json => {
+  //       console.log(json)
+  //     })
+  // }, [])
+
+  const fetchWeather = async () => {
+    const data = await fetch(
+      'https://api.darksky.net/forecast/231063ebc5ecb280ea352c370e75450b/37.8267,-122.4233'
+    )
+    const response = await data.json()
+    console.log(response)
+  }
+  useEffect(() => {
+    // fetchWeather()
+  })
+
+  return (
+    <View>
+      <Button
+        title='Add a city'
+        onPress={() => navigation.navigate('AddCityScreen')}
+      />
+      <Button
+        title='Test Profile Page'
+        onPress={() =>
+          navigation.navigate('CityForecast', {
+            test: 'Cool weather data here'
+          })
+        }
+      />
+    </View>
+  )
+}
+
+HomeScreen.navigationOptions = {
+  title: 'Home'
+}
