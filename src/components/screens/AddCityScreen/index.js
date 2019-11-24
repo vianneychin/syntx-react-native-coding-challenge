24c7de66b9c5ebace2df2593a1ec6e26f56d6c4a
Vianney <vianneyjchin@gmail.com>
2019-11-23 21:02:04 -0800

    Remove item from list of cities

--- src/components/screens/AddCityScreen/index.js
+++ src/components/screens/AddCityScreen/index.js
@@ -3,21 +3,22 @@ import { AddCityContainer, City, CityButton } from '../../../styles'
 import { data, moreData } from '../../../data'
 
 export const AddCityScreen = ({ navigation }) => {
+  const [extraData, setExtraData] = useState(moreData)
+
   const getParams = param => {
     return navigation.getParam(param, 'data')
   }
-  const handlePress = item => {
-    console.log(item)
+  const handlePress = (item, index) => {
     const hardData = getParams('hardData')
     const setHardData = getParams('setHardData')
-    console.log(hardData)
     setHardData([...hardData, item])
+    setExtraData(extraData => extraData.filter(value => item !== value))
   }
 
   const renderData = () => {
-    return moreData.map(item => {
+    return extraData.map((item, index) => {
       return (
-        <CityButton key={item.id} onPress={() => handlePress(item)}>
+        <CityButton key={item.id} onPress={() => handlePress(item, index)}>
           <City>{item.city}</City>
         </CityButton>
       )
